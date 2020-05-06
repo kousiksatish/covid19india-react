aff0221523174ab5bdab602f14cf1b66db42e18a
kousiksatish <kousiksatish@users.noreply.github.com>
2020-05-06 05:58:20 +0000

    Deploying to gh-pages from  @ ce37da18f9d541ff265919e50caa45009a60343e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6cda6e680f37e49bcfe4682525a810d1.js"
+  "/precache-manifest.451782ece782c47eeb6df264dd7385bc.js"
 );
 
 self.addEventListener('message', (event) => {
